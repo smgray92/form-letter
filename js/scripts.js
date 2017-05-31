5b9431fc3edad2a995a12af785bfff9e3b051b95
Sharon Gray <smgray113@gmail.com>
2017-05-30 19:37:30 -0700

    Create form letter html that practices using javascript and forms.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+  $(document).ready(function() {
+    $(".blanks form").submit(function(event) {
+    var nameInput = $("input#name").val();
+    $(".name").text(nameInput);
+
+    $("#letter").show();
+
+    event.preventDefault();
+  });
+  });
